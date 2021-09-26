ecd998658c4c03aecb4e3ca3c12fee56e2454397
daniellybx <danielly.xavier@outlook.com>
2021-09-26 17:41:51 -0300

    Atualização do script 4.

--- scripts/04_estatisticas_descritivas.R
+++ scripts/04_estatisticas_descritivas.R
@@ -2,10 +2,8 @@
 library(tidyverse) #pacote para a manipulação dos dados
 
 ## datasets de dados 
-p = "dados/"
-
-esus = read.csv2(paste0(p, "20210601_dadosesus_df.csv")) #importando dados do esus df de 01/06/2021
-sivep = read.csv2(paste0(p, "20210823_dadossivep.csv")) #importando dados do esus df de 23/08/2021
+esus = read.csv2(paste0("dados/20210601_dadosesus_df.csv")) #importando dados do esus df de 01/06/2021
+sivep = read.csv2(paste0("dados/20210823_dadossivep.csv")) #importando dados do esus df de 23/08/2021
 
 source("scripts/03_aula_importando_dados.R") #realizando os tratamentos do script 03
 
@@ -27,5 +25,6 @@ sqrt(var(esus$idade)) #desvio padrão da idade
 sqrt(var(esus$idade)) == sd(esus$idade) #testando se os dados são iguais
 
 ## calculando medidas descritivas para dados categóricos
+summary(sivep$CS_SEXO)
 table(sivep$CS_SEXO) #tabela de frequencia de dados
 tabela01 = data.frame(table(sivep$CS_SEXO)) #tabela de frequencia de dados em data.frame
